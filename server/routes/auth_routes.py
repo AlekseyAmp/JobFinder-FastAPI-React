c0786d29572fed04aefe381f2a97b78668901842
AlekseyAmp <thientaovv@gmail.com>
2023-05-25 10:17:13 +0500

    add routes

---
+++ server/routes/auth_routes.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Response, Depends
+from sqlalchemy.orm import Session
+
+from config.database import get_db
+from config.jwt_config import AuthJWT
+from schemas.auth_schema import Register, Login
+from services.user_services import get_user_id
+from services import auth_services as a_s
+
+
+router = APIRouter()
+
+
+@router.post("/auth/register")
+async def register(data: Register, response: Response, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    return await a_s.create_new_user(data, response, db, authorize)
+
+
+@router.post("/auth/login")
+async def login(data: Login, response: Response, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    return await a_s.login_user(data, response, db, authorize)
+
+
+@router.get("/auth/refresh_token")
+async def refresh_token(response: Response, authorize: AuthJWT = Depends(), user_id: str = Depends(get_user_id)):
+    return await a_s.refresh_token(authorize, response, user_id)
+
+
+@router.get("/auth/logout")
+async def logout(response: Response, authorize: AuthJWT = Depends()):
+    return await a_s.logout_user(response, authorize)
