2e074eee6ae967a11c14afdd599bde4f5ab3b000
AlekseyAmp <thientaovv@gmail.com>
2023-05-25 10:16:14 +0500

    change import settings

--- server/config/database.py
+++ server/config/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-from settings import settings
+from config.settings import settings
 
 
 engine = create_engine(settings.DATABASE_URL)
