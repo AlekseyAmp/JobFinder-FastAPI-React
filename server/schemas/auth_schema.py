dd7eebd05b45c7b060ad504cfede9907d85371c0
AlekseyAmp <thientaovv@gmail.com>
2023-05-25 12:49:39 +0500

    delete extra spaces

--- server/schemas/auth_schema.py
+++ server/schemas/auth_schema.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class Register(BaseModel):
     name: str
     surname: str
-    email: str
     phone_number: str
+    email: str
     password: str
 
     class Config:
