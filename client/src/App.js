04871e3d755469a74db7e920cd623fd269f72512
AlekseyAmp <thientaovv@gmail.com>
2023-05-26 12:22:45 +0500

    add login in route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import routes from './routes';
 
 import './assets/reset.scss';
 import './assets/global.scss';
 
+import Header from './components/Header/Header';
 import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,14 @@
 import Register from './pages/Auth/Register'
+import Login from './pages/Auth/Login'
 
 const routes = [
     {
         path: '/register',
         component: Register,
+    },
+    {
+        path: '/login',
+        component: Login,
     },
 ]
 export default routes;
