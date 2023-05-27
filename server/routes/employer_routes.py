03b1f1071feb1919053563373f8c2a8402d07023
AlekseyAmp <thientaovv@gmail.com>
2023-05-27 12:40:01 +0500

    Add employer_routes(create, get, change)

---
+++ server/routes/employer_routes.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from config.database import get_db
+from schemas.employer_schema import EmployerData
+from services.user_services import get_user_id
+from services import employer_services as e_s
+
+
+router = APIRouter()
+
+
+@router.post("/employers/create")
+async def create_employer(data: EmployerData, db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
+    return await e_s.create_employer(data, db, user_id)
+
+
+@router.get("/employers")
+async def get_all_employers(db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
+    return await e_s.get_employers(db, user_id)
+
+
+@router.patch("/employers/{employer_id}")
+async def confirm_employer(employer_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
+    return await e_s.confirm_employer(employer_id, db, user_id)
