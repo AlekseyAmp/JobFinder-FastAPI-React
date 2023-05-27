3196409dbbdb33c693d765610c48307ebbe04211
AlekseyAmp <thientaovv@gmail.com>
2023-05-27 12:36:17 +0500

    make unloading routes from map

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,24 @@ import { Routes, Route } from 'react-router-dom';
 import './assets/reset.scss';
 import './assets/global.scss';
 
+import routes from './routes';
+
 import Header from './components/Header/Header';
-import Register from './pages/Auth/Register';
-import Login from './pages/Auth/Login';
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={
+              <route.component />
+            }
+          />
+        ))}
       </Routes>
     </div>
   );
