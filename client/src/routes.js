9ae8f9007a4d7571a49e4705da56cb34b823511f
AlekseyAmp <thientaovv@gmail.com>
2023-05-27 12:36:30 +0500

    add new routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,13 @@
+import Main from './pages/Main/Main';
 import Register from './pages/Auth/Register'
 import Login from './pages/Auth/Login'
+import Employer from './pages/Employer/Employer';
 
 const routes = [
+    {
+        path: '/',
+        component: Main,
+    },
     {
         path: '/register',
         component: Register,
@@ -9,6 +15,10 @@ const routes = [
     {
         path: '/login',
         component: Login,
+    },
+    {
+        path: '/employer',
+        component: Employer,
     },
 ]
 export default routes;
