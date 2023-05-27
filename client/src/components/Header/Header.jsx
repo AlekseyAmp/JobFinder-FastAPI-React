46630b28fd91d463242566c25d3814712badbf5d
AlekseyAmp <thientaovv@gmail.com>
2023-05-27 12:37:29 +0500

    add in header authorize chekced

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { access_token } from '../../constants/token';
+import axios from '../../utils/axios';
 
 import styles from './Header.module.scss';
 import '../../assets/variables.scss';
@@ -10,8 +11,27 @@ import Logout from '../../pages/Auth/Logout';
 
 function Header() {
   const isAuthorized = !!access_token;
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
 
-  console.log(isAuthorized)
+  useEffect(() => {
+    async function getNameSurname() {
+      try {
+        const response = await axios.get(`/users/me`);
+
+        if (response.data) {
+          setName(response.data.name);
+          setSurname(response.data.surname);
+        }
+      } catch (error) {
+        console.log(error.response.data.detail);
+      }
+    }
+    if (isAuthorized) {
+      getNameSurname();
+    }
+  }, [isAuthorized]);
+
   return (
     <div className={styles.header}>
       <Link to='/' className={styles.logo}>
@@ -22,10 +42,10 @@ function Header() {
       <div className={styles.menu}>
         <ul>
           <li>
-            <Link to='/' className={`link-text`}>Работодателям</Link>
+            <Link to='/applicant' className={`link-text`}>Соискателям</Link>
           </li>
           <li>
-            <Link to='/' className={`link-text`}>Соискателям</Link>
+            <Link to='/employer' className={`link-text`}>Работодателям</Link>
           </li>
         </ul>
       </div>
@@ -33,11 +53,22 @@ function Header() {
       <div className={styles.menu}>
         <ul>
           {isAuthorized ? (
-            <Logout />
+            <>
+              <li>
+                <Link to='/' className={`link-text`}>{name} {surname}</Link>
+              </li>
+              <li>
+                <Logout />
+              </li>
+            </>
           ) : (
             <>
-              <Link to='/login' className={`link-text`}>Вход</Link>
-              <Link to='/register' className={`link-text`}>Регистрация</Link>
+              <li>
+                <Link to='/login' className={`link-text`}>Вход</Link>
+              </li>
+              <li>
+                <Link to='/register' className={`link-text`}>Регистрация</Link>
+              </li>
             </>
           )}
         </ul>
