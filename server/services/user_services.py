2410f6c40c5bde9e1acfcfa87a5fcf525203797b
AlekseyAmp <thientaovv@gmail.com>
2023-05-28 18:43:45 +0500

    changes

--- server/services/user_services.py
+++ server/services/user_services.py
@@ -28,12 +28,11 @@ async def get_user_info(db: Session, user_id: str):
 
 
 async def get_user_id(authorize: AuthJWT = Depends()):
-    try:
-        authorize.jwt_required()
-        user_id = authorize.get_jwt_subject()
-        return user_id
-    except (MissingTokenError):
+    if MissingTokenError:
         raise HTTPException(
             status_code=401,
             detail="Not authorized"
         )
+    authorize.jwt_required()
+    user_id = authorize.get_jwt_subject()
+    return user_id
