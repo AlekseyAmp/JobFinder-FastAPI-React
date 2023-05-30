41f564e05c302f46b530048c8db04b584952aeeb
AlekseyAmp <thientaovv@gmail.com>
2023-05-30 22:44:03 +0500

    Add new delete route

--- server/routes/employer_routes.py
+++ server/routes/employer_routes.py
@@ -16,10 +16,15 @@ async def create_employer(data: EmployerData, db: Session = Depends(get_db), use
 
 
 @router.get("/employers")
-async def get_all_employers(db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
-    return await e_s.get_all_employers(db, user_id)
+async def get_all_employers(db: Session = Depends(get_db)):
+    return await e_s.get_all_employers(db)
 
 
 @router.patch("/employers/{employer_id}")
 async def confirm_employer(employer_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
     return await e_s.confirm_employer(employer_id, db, user_id)
+
+
+@router.delete("/employers/{employer_id}")
+async def delete_employer(employer_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
+    return await e_s.delete_employer(employer_id, db, user_id)
