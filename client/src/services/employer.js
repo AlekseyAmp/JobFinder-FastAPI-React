1415799548a236ef97f235e6c33b8b72c5a62646
AlekseyAmp <thientaovv@gmail.com>
2023-05-30 22:42:51 +0500

    Add new functions in employer service

--- client/src/services/employer.js
+++ client/src/services/employer.js
@@ -24,14 +24,36 @@ export async function getAllEmployers() {
     }
 }
 
-export async function confirmEmployer(employer_id) {
+export async function confirmEmployer(employer_id, employers, setEmployers) {
     try {
         const response = await axios.patch(`/employers/${employer_id}`);
 
         if (response.data) {
-            return response.data;
+            setEmployers(employers.filter(employer => employer.id !== employer_id));
+            const updatedEmployers = employers.map((employer) => {
+                if (employer.id === employer_id) {
+                    return { ...employer, is_confirmed: true };
+                }
+                return employer;
+            });
+            setEmployers(updatedEmployers);
+            console.log(response.data);
         }
     } catch (error) {
         console.log(error.response.data.detail);
     }
 }
+
+export async function deleteEmployer(employer_id, employers, setEmployers) {
+    try {
+      const response = await axios.delete(`/employers/${employer_id}`);
+      if (response.data) {
+        setEmployers(employers.filter(employer => employer.id !== employer_id));
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.log(error.response.data.detail);
+    }
+  }
+  
+
