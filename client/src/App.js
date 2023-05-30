723c18e2afe1b5b8f39c4dac6ca3561312eaf217
AlekseyAmp <thientaovv@gmail.com>
2023-05-30 22:40:17 +0500

    Offloading routes via map

--- client/src/App.js
+++ client/src/App.js
@@ -4,40 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import './assets/reset.scss';
 import './assets/global.scss';
 
+import routes from './routes';
+
 import Header from './components/Header/Header';
-import AuthChecked from './components/Checked/AuthChecked';
-import AdminChecked from './components/Checked/AdminChecked';
-import Admin from './pages/Admin/Admin';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import Main from './pages/Main/Main'
-import Employer from './pages/Employer/Employer';
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Routes>
-        <Route
-          path="/login"
-          element={<AuthChecked><Login /></AuthChecked>}
-        />
-        <Route
-          path="/register"
-          element={<AuthChecked><Register /></AuthChecked>}
-        />
-        <Route
-          path="/"
-          element={<Main />}
-        />
-        <Route
-          path="/employer"
-          element={<AuthChecked><Employer /></AuthChecked>}
-        />
-        <Route
-          path="/admin"
-          element={<AuthChecked><AdminChecked><Admin /></AdminChecked></AuthChecked>}
-        />
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={<route.component />} />
+        ))}
       </Routes>
     </div>
   );
