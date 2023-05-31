d5810f18ef5fe114328931c71df8b6dade29aa07
AlekseyAmp <thientaovv@gmail.com>
2023-05-31 12:50:49 +0500

    Change route /employers/create on POST /employer

--- server/routes/employer_routes.py
+++ server/routes/employer_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from config.database import get_db
-from schemas.employer_schema import EmployerData
+from schemas.employer_schema import EmployerForm
 from services.user_services import get_user_id
 from services import employer_services as e_s
 
@@ -10,8 +10,8 @@ from services import employer_services as e_s
 router = APIRouter()
 
 
-@router.post("/employers/create")
-async def create_employer(data: EmployerData, db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
+@router.post("/employers")
+async def create_employer(data: EmployerForm, db: Session = Depends(get_db), user_id: str = Depends(get_user_id)):
     return await e_s.create_employer(data, db, user_id)
 
 
