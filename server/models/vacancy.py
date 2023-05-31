c82ba3bf037746b170221e5e183da57365bec564
AlekseyAmp <thientaovv@gmail.com>
2023-05-31 12:49:22 +0500

    Add new model vacancy

---
+++ server/models/vacancy.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ARRAY, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from config.database import Base
+
+
+class Vacancy(Base):
+    __tablename__ = 'vacancies'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255))
+    description = Column(String)
+    place = Column(String(255))
+    salary = Column(String(50))
+    tags = Column(ARRAY(String(50)))
+    is_confirmed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now)
+
+    employer_id = Column(Integer, ForeignKey('employers.id'))
+    employer = relationship("Employer", back_populates="vacancy")
