ddeb185883fb9763a6c49cfe0b390a6d9b72744f
AlekseyAmp <thientaovv@gmail.com>
2023-06-02 20:29:53 +0500

    Renamed files

--- server/models/feedback.py
+++ server/models/feedback.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
